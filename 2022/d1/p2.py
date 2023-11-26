34a0b74072856ab3ec0f6641261a3993d67f46e2
Orange Slices <crispy.orange.slices@gmail.com>
2023-11-25 17:29:22 -0800

    22d1p2

---
+++ 2022/d1/p2.py
@@ -0,0 +1,25 @@
+import sys
+
+def main():
+    filename = sys.argv[1]
+    print(filename)
+    with open(filename) as f:
+        content = f.readlines()
+        elf_totals = []
+        elf_totals.append(0)
+        for line in content:
+            line = line.strip()
+            if line == "":
+                elf_totals.append(0)
+            else:
+               elf_totals[-1] += int(line) 
+
+        se = sorted(enumerate(elf_totals), key=lambda i: i[1], reverse=True)
+        top_three = 0
+        for idx, amt in se[:3]:
+            top_three += amt
+        print(top_three)
+            
+
+if __name__ == "__main__":
+    main()
